ee3838f37cb07cb907e76147d1d3fd11c6892006
ANDRES LOBO ROCA <s184590@student.hb.se>
2021-05-26 21:30:07 +0200

    Tests

---
+++ FoodTrackerTests/AddViewModelsTests.cs
@@ -0,0 +1,22 @@
+﻿using FoodTracker.Models;
+using FoodTracker.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FoodTrackerTests
+{
+    [TestClass]
+    public class AddViewModelsTests
+    {
+        [TestMethod]
+        public void OnAddItemTest()
+        {
+            //Arrange
+            AddViewModel addViewModel = new AddViewModel();
+            //Act
+            addViewModel.OnAddItem();
+            //Assert
+            Assert.IsFalse(addViewModel.isSuccessfull);
+        }
+    }
+}
---
+++ FoodTrackerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FoodTrackerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FoodTrackerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f81c0032-cc93-4124-aa78-fd38a001de47")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
